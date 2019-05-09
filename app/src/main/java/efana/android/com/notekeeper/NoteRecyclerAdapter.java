9e15ec0038f7d3613de8fa80965d19ece6ca96de
Efana Usua <usuaefana@gmail.com>
2019-05-09 12:36:07 +0100

    Finished RecyclerView

--- app/src/main/java/efana/android/com/notekeeper/NoteRecyclerAdapter.java
+++ app/src/main/java/efana/android/com/notekeeper/NoteRecyclerAdapter.java
@@ -1,10 +1,56 @@
 package efana.android.com.notekeeper;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class NoteRecyclerAdapter {
+import java.util.List;
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder> {
+
+    private final Context mContext;
+    private final List<NoteInfo> mNotes;
+    private final LayoutInflater mLayoutInflater;
+
+    public NoteRecyclerAdapter(Context context, List<NoteInfo> notes) {
+        mContext = context;
+        mNotes = notes;
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mLayoutInflater.inflate(R.layout.item_note_list, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        NoteInfo note = mNotes.get(position);
+        holder.mTextCourse.setText(note.getCourse().getTitle());
+        holder.mTextTitle.setText(note.getTitle());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        
+
+        public final TextView mTextCourse;
+        public final TextView mTextTitle;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextCourse = (TextView) itemView.findViewById(R.id.text_course);
+            mTextTitle = (TextView) itemView.findViewById(R.id.text_title);
+        }
     }
 }
