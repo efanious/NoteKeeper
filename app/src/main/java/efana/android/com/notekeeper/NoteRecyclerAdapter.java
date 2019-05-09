b5997881347ce09c3c3004ffde7d2644839dea8a
Efana Usua <usuaefana@gmail.com>
2019-05-09 11:16:59 +0100

    Added some files

---
+++ app/src/main/java/efana/android/com/notekeeper/NoteRecyclerAdapter.java
@@ -0,0 +1,10 @@
+package efana.android.com.notekeeper;
+
+import android.support.v7.widget.RecyclerView;
+
+public class NoteRecyclerAdapter {
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        
+    }
+}
