edab9318238b8388a34fd4290c880cfccb7f937e
Efana Usua <usuaefana@gmail.com>
2019-05-12 18:46:58 +0100

    Created contract,table and open helper

--- app/src/main/java/efana/android/com/notekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/efana/android/com/notekeeper/NoteKeeperDatabaseContract.java
@@ -1,21 +1,24 @@
 package efana.android.com.notekeeper;
 
+import android.provider.BaseColumns;
+
 public final class NoteKeeperDatabaseContract {
 
     private NoteKeeperDatabaseContract(){}
 
-    public static final class CourseInfoEntry {
+    public static final class CourseInfoEntry implements BaseColumns {
         public static final String TABLE_NAME = "course_info";
         public static final String COLUMN_COURSE_ID = "course_id";
         public static final String COLUMN_COURSE_TITLE = "course_title";
 
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " (" +
-                        COLUMN_COURSE_ID + ", " +
-                        COLUMN_COURSE_TITLE + ")";
+                        _ID + " INTEGER PRIMARY KEY, " +
+                        COLUMN_COURSE_ID + " TEXT UNIQUE NOT NULL, " +
+                        COLUMN_COURSE_TITLE + " TEXT NOT NULL)";
     }
 
-    public static final class NoteInfoEntry {
+    public static final class NoteInfoEntry implements BaseColumns {
         public static final String TABLE_NAME = "note_info";
         public static final String COLUMN_NOTE_TITLE = "note_title";
         public static final String COLUMN_NOTE_TEXT = "note_text";
@@ -23,8 +26,9 @@ public final class NoteKeeperDatabaseContract {
 
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " (" +
-                        COLUMN_NOTE_TITLE + ", " +
-                        COLUMN_NOTE_TEXT + ", " +
-                        COLUMN_COURSE_ID + ")";
+                        _ID + " INTEGER PRIMARY KEY, " +
+                        COLUMN_NOTE_TITLE + " TEXT NOT NULL, " +
+                        COLUMN_NOTE_TEXT + " TEXT, " +
+                        COLUMN_COURSE_ID + " TEXT NOT NULL)";
     }
 }
